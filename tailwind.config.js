2d0de57f9c8abfd81395cfaac0c88d55025f5b76
jmd971 <jdolmare@gmail.com>
2026-04-04 00:22:13 -0400

    Remove TypeScript type annotation from Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import type { Config } from 'tailwindcss'
-
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
@@ -30,4 +28,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config
+}
